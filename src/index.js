126ad16ffa4dc22b4bb29ad63a8c8c473e4310c7
LevelDrain <drain2291@gmail.com>
2021-03-27 00:59:06 +0900

    three.js環境の完成

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-import { hello } from "./sub";
+import * as THREE from "three";
+import "core-js"; // IEで動作するようにポリフィルを導入
 
-const message = "Hello World！！";
+window.addEventListener("DOMContentLoaded", () => {
+  const $canvas = document.querySelector('#Canvas'),
+    width = window.innerWidth,
+    height = window.innerHeight;
 
-// sub.jsに定義されたJavaScriptを実行する。
-hello(message);
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 2000);
+  camera.position.z = 3;
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: $canvas
+  });
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshToonMaterial({
+    color: 0xf066aa
+  });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.intensity = 1;
+  light.position.set(1, 1, 1);
+  scene.add(light);
+
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(devicePixelRatio);
+  renderer.render(scene, camera);
+
+  const tick = () => {
+    cube.rotation.x += 0.02;
+    cube.rotation.y += 0.02;
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  }
+  tick();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: "production",
-
-  // ローカル開発用環境を立ち上げる
-  // 実行時にブラウザが自動的に localhost を開く
+  //mode: "production",
+  mode: "development",
   devServer: {
     contentBase: "dist",
     open: true
   },
 
-  // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: "./src/index.js",
-
+  entry: ["@babel/polyfill", "./src/index.js"],
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: "main.js",
+  },
   module: {
     rules: [{
       // 拡張子 .js の場合
@@ -24,7 +25,13 @@ module.exports = {
         options: {
           presets: [
             // プリセットを指定することで、ES2020 を ES5 に変換
-            "@babel/preset-env",
+            [
+              "@babel/preset-env",
+              {
+                useBuiltIns: "entry",
+                corejs: 3,
+              },
+            ],
           ],
         },
       }, ],
