da98061016373ddcfcbeb7056b65f176169cfe0e
LevelDrain <drain2291@gmail.com>
2021-04-06 14:32:27 +0900

    魔法陣生成コードを移植

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,64 @@
 import * as THREE from 'three';
 import 'core-js'; // IEで動作するようにポリフィルを導入
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-window.addEventListener('DOMContentLoaded', () => {
-  const $canvas = document.querySelector('#Canvas'),
-    width = window.innerWidth,
-    height = window.innerHeight;
+const drawMagicCircle = () => {
+  const $canvas = document.querySelector('.EX1'),
+    width = $canvas.clientWidth,
+    height = $canvas.clientHeight;
+
+  if ($canvas.width !== width || $canvas.height !== height) {
+    // サイズが違っていたら、描画バッファーのサイズを表示サイズと同じサイズに合わせる。
+    $canvas.width = width;
+    $canvas.height = height;
+  }
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 2000);
-  camera.position.z = 3;
+  camera.position.z = 20;
 
   const renderer = new THREE.WebGLRenderer({
     canvas: $canvas,
   });
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshToonMaterial({
-    color: 0xf066aa,
-  });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  const loader = new THREE.TextureLoader();
+
+  const magicCircle = new THREE.Mesh(
+    new THREE.CircleGeometry(8, 32),
+    new THREE.MeshBasicMaterial({
+      map: loader.load('./img/img_marchosias.png'),
+      side: THREE.DoubleSide,
+      transparent: true,
+    })
+  );
+  magicCircle.rotation.x = -Math.PI / 2.5;
+  scene.add(magicCircle);
+
   const light = new THREE.DirectionalLight(0xffffff);
   light.intensity = 1;
-  light.position.set(1, 1, 1);
+  light.position.set([1, 1, 1]);
   scene.add(light);
+
+  //デバッグ用
+  let controls = new OrbitControls(camera, $canvas);
+  controls.enableDamping = true;
+  controls.autoRotate = true;
+  controls.autoRotateSpeed = 1;
+  controls.update();
 
   renderer.setSize(width, height);
   renderer.setPixelRatio(devicePixelRatio);
-  renderer.render(scene, camera);
-
   const tick = () => {
-    cube.rotation.x += 0.02;
-    cube.rotation.y += 0.02;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     renderer.render(scene, camera);
     requestAnimationFrame(tick);
   };
   tick();
-});
+};
+
+const init = () => {
+  drawMagicCircle();
+};
+
+onload = init;
